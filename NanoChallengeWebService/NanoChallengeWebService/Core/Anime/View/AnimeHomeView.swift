1de86a0eab9cff44cc7bc9027ce0ca2fa7972123
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-08-17 15:26:33 -0300

    create anime folder

---
+++ NanoChallengeWebService/NanoChallengeWebService/Core/Anime/View/AnimeHomeView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimeHomeView.swift
+//  NanoChallengeWebService
+//
+//  Created by Eduardo on 17/08/23.
+//
+
+import SwiftUI
+
+struct AnimeHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimeHomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimeHomeView()
+    }
+}
