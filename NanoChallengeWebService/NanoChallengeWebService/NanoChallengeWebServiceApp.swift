e8050068f28150973b1e297163e3eeeec0b58ba9
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-08-17 15:04:01 -0300

    First Doc

---
+++ NanoChallengeWebService/NanoChallengeWebService/NanoChallengeWebServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallengeWebServiceApp.swift
+//  NanoChallengeWebService
+//
+//  Created by Eduardo on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallengeWebServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
