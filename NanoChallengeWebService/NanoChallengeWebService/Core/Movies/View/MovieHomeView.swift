593b1b8ddb8314eb5def44738aae1f60517ebbd6
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2023-08-26 12:10:06 -0300

    Modularização da MovieListView

--- NanoChallengeWebService/NanoChallengeWebService/Core/Movies/View/MovieHomeView.swift
+++ NanoChallengeWebService/NanoChallengeWebService/Core/Movies/View/MovieHomeView.swift
@@ -5,173 +5,41 @@ enum Strings: String {
     case titleError = "Title Error"
 }
 
-struct MovieHomeView: View {
+///É o header das ScrollMovies, recebe como pariametro o nome da ScrollView, normalmente o gênero da Lista de filmes.
+struct HearderSrollMovie_Component: View {
     
-    @State var responseMovieDiscover: responceDiscoverMovies?
-    @State var movieService: MovieService = MovieService()
-    
-    /*
-     struct PopularTeste: View {
-         @StateObject var seriesVM = SeriesViewModel()
-         var body: some View {
-             VStack {
-                 NavigationStack{
-                     ScrollView {
-                         VStack {
-                             HStack {
-                                 Text("Series")
-                                     .font(.title)
-                                     .padding(.horizontal)
-                                 Spacer()
-                                 Text("Ver todas")
-                                     .font(.title2)
-                                     .padding(.horizontal)
-                             }
-                             ScrollView(.horizontal, showsIndicators: false){
-                                 LazyHStack {
-                                     ForEach(0 ..< seriesVM.tvSeriesArray.count, id: \.self){ index in
-                                         NavigationLink {
-                                             TVSerieDetail(series: seriesVM.tvSeriesArray[index])
-                                             
-                                         } label: {
-                                             VStack {
-                                                 // if let para pegar o complemento da imagem do link
-                                                 if let backDropPath = seriesVM.tvSeriesArray[index].backdrop_path {
-                                                     AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/original\(backDropPath)")) {
-                                                         image in
-                                                         image
-                                                             .resizable()
-                                                             .scaledToFill()
-                                                             .backgroundStyle(.white)
-                                                             .frame(maxWidth: 185,maxHeight: 230)
-                                                             .cornerRadius(16)
-                                                             .shadow(radius: 8, x: 5, y: 5)
-                                                             .overlay {
-                                                                 RoundedRectangle(cornerRadius: 16)
-                                                                     .stroke(.gray.opacity(0.5), lineWidth: 1)
-                                                             }
-                                                     } placeholder: {
-                                                         RoundedRectangle(cornerRadius: 16)
-                                                             .stroke(.gray.opacity(0.5), lineWidth: 1)
-                                                             .frame(width: 160, height: 230)
-                                                             .overlay {
-                                                                 ProgressView()
-                                                                     .scaleEffect(4)
-                                                                     .frame(maxHeight: 96)
-                                                                     .padding()
-                                                             }
-                                                     }
-                                                     
-                                                 }
-                                                 Text("\(seriesVM.tvSeriesArray[index].name.capitalized)")
-                                                     .font(.title2)
-                                                     .foregroundColor(.black)
-                                                 Text("\(seriesVM.count2)")
-                                                 
-                                             }
-                                             .padding()
-                                             
-                                         } // MARK: LABEL
-                                         
-                                     }
-                                 }
-                             }
-                             
-                         } // MARK: VSTACK SERIES
-                         .navigationTitle("Series")
-                     }
-                     
-                 }
-                 
-                 .task {
-                     await seriesVM.fetchAllTVSeries()
-                 }
-                 .ignoresSafeArea()
-             } // MARK: Primeira VSTACK
-             .ignoresSafeArea()
-             
-         }
-     }
-     */
+    var title: String
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack {
-                    HStack {
-                        Text("Movies")
-                            .font(.title)
-                            .padding(.horizontal)
-                        Spacer()
-                        Text("View All")
-                            .font(.title2)
-                            .padding(.horizontal)
-                    }
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack {
-                            if let responseMovieDiscover = responseMovieDiscover {
-                                ForEach(responseMovieDiscover.results ?? [], id: \.id) { movie in
-                                    NavigationLink(destination: MovieItem(idMovie: movie.id)) {
-                                        VStack {
-//                                            if let backDropPath = movie.backdropPath {
-                                            //https://image.tmdb.org/t/p/original/vB8o2p4ETnrfiWEgVxHmHWP9yRl.jpg
-//                                            if let backDropPath = movie.backdropPath {
-                                            AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/original\(movie.backdropPath ?? "/vB8o2p4ETnrfiWEgVxHmHWP9yRl.jpg")")) { image in
-                                                   image
-                                                       .resizable()
-                                                       .aspectRatio(contentMode: .fill) // Use fill here
-                                                       .frame(maxWidth: 185, maxHeight: 230)
-                                                       .cornerRadius(16)
-//                                                       .shadow(radius: 8, x: 5, y: 5)
-//                                                       .overlay(
-//                                                           RoundedRectangle(cornerRadius: 16)
-//                                                               .stroke(Color.gray.opacity(0.5), lineWidth: 1)
-//                                                       )
-                                               } placeholder: {
-                                                   ProgressView()
-                                                       .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                                                       .scaleEffect(4)
-                                                       .frame(maxHeight: 96)
-                                               }
-//                                           }
-//                                            } else {
-//                                                Text("N tem imagem essa budega 🖼️")
-//                                            }
-                                            //                                        AsyncImage(url: URL(string: "https://www.example.com/sample-image.jpg")) { image in
-                                            //                                            image
-                                            //                                                .resizable()
-                                            //                                                .scaledToFill()
-                                            //                                                .frame(maxWidth: 185, maxHeight: 230)
-                                            //                                                .cornerRadius(16)
-                                            //                                        }
-                                            //                                        .placeholder {
-                                            //                                            Color.gray.frame(width: 160, height: 230)
-                                            //                                        }
-                                            
-                                            
-                                            //                                    } else {
-                                            //                                        Text(Strings.imageError.rawValue)
-                                            //                                    }
-                                            
-                                            if let title = movie.title {
-                                                Text(title)
-                                            } else {
-                                                Text(Strings.titleError.rawValue)
-                                            }
-                                        }
-                                        .padding()
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                .navigationTitle("Movies")
-                .task {
-                    do {
-                        responseMovieDiscover = try await movieService.getDiscoverMovies()
-                    } catch {
-                        print("Error: \(error)")
+        HStack {
+            Text(title)
+                .font(.title)
+                .padding(.horizontal)
+            Spacer()
+            Button {
+                
+            } label: {
+                Text("View All")
+                    .font(.title2)
+                    .foregroundColor(.black)
+            }
+            .padding()
+
+           
+        }
+    }
+}
+
+///Componente que exibe horizontalmente uma lista de Filmes, mostrando o título e banner deles
+struct ScrollMovie_Component: View {
+    let movies: [Movie]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack {
+                ForEach(movies) { movie in
+                    NavigationLink(destination: MovieItem(idMovie: movie.id)) {
+                        MovieItemViewComponent(movie: movie)
                     }
                 }
             }
@@ -179,6 +47,74 @@ struct MovieHomeView: View {
     }
 }
 
+///Componente que configura a aparência dos filmes na ScrollMovieComponent
+struct MovieItemViewComponent: View {
+    
+    let movie: Movie
+    
+    //TODO: Colocar placeholder com animação de carregamento
+    var body: some View {
+        VStack {
+            if let backDropPath = movie.backdropPath {
+                AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/original\(backDropPath)")) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: 185, maxHeight: 230)
+                        .cornerRadius(16)
+                        .shadow(radius: 8, x: 5, y: 5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                        )
+                } placeholder: {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                        .scaleEffect(4)
+                        .frame(maxHeight: 96)
+                }
+            }
+            
+            if let title = movie.title {
+                Text(title)
+            } else {
+                Text(Strings.titleError.rawValue)
+            }
+        }
+        .padding()
+    }
+}
+
+///Vier que exibe os filmes
+struct MovieHomeView: View {
+    
+    @State var responseMovieDiscover: responceDiscoverMovies?
+    @State var movieService: MovieService = MovieService()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack {
+                    VStack {
+                        HearderSrollMovie_Component(title: "Movies")
+                        ScrollMovie_Component(movies: responseMovieDiscover?.results ?? [])
+                    }
+                    .task {
+                        do {
+                            responseMovieDiscover = try await movieService.getDiscoverMovies()
+                        } catch {
+                            print("Error: \(error)")
+                        }
+                    }
+                    
+                    
+                }
+                .navigationTitle("Movies")
+                
+            }
+        }
+    }
+}
 struct MovieHomeView_Previews: PreviewProvider {
     static var previews: some View {
         MovieHomeView()
