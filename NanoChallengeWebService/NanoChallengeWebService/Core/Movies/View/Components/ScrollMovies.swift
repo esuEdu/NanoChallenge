d50484fb8615237f458193cb0b18da156dd739ff
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2023-08-26 12:49:14 -0300

    Problema de tamanho das imagens resolvido

--- NanoChallengeWebService/NanoChallengeWebService/Core/Movies/View/Components/ScrollMovies.swift
+++ NanoChallengeWebService/NanoChallengeWebService/Core/Movies/View/Components/ScrollMovies.swift
@@ -15,7 +15,9 @@ struct ScrollMovies: View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack {
                 ForEach(movies) { movie in
-                    NavigationLink(destination: MovieItem(idMovie: movie.id)) {
+                    NavigationLink {
+                        MovieItem(idMovie: movie.id)
+                    } label: {
                         MovieDesignScrollView(movie: movie)
                     }
                 }
@@ -23,6 +25,72 @@ struct ScrollMovies: View {
         }
     }
 }
+/*
+ var body: some View {
+ 
+                         ScrollView(.horizontal, showsIndicators: false){
+                             LazyHStack {
+                                 ForEach(0 ..< seriesVM.tvSeriesArray.count, id: \.self){ index in
+                                     NavigationLink {
+                                         TVSerieDetail(series: seriesVM.tvSeriesArray[index])
+                                         
+                                     } label: {
+                                         VStack {
+                                             // if let para pegar o complemento da imagem do link
+                                             if let backDropPath = seriesVM.tvSeriesArray[index].backdrop_path {
+                                                 AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/original\(backDropPath)")) {
+                                                     image in
+                                                     image
+                                                         .resizable()
+                                                         .scaledToFill()
+                                                         .backgroundStyle(.white)
+                                                         .frame(maxWidth: 185,maxHeight: 230)
+                                                         .cornerRadius(16)
+                                                         .shadow(radius: 8, x: 5, y: 5)
+                                                         .overlay {
+                                                             RoundedRectangle(cornerRadius: 16)
+                                                                 .stroke(.gray.opacity(0.5), lineWidth: 1)
+                                                         }
+                                                 } placeholder: {
+                                                     RoundedRectangle(cornerRadius: 16)
+                                                         .stroke(.gray.opacity(0.5), lineWidth: 1)
+                                                         .frame(width: 160, height: 230)
+                                                         .overlay {
+                                                             ProgressView()
+                                                                 .scaleEffect(4)
+                                                                 .frame(maxHeight: 96)
+                                                                 .padding()
+                                                         }
+                                                 }
+                                                 
+                                             }
+                                             Text("\(seriesVM.tvSeriesArray[index].name.capitalized)")
+                                                 .font(.title2)
+                                                 .foregroundColor(.black)
+                                             Text("\(seriesVM.count2)")
+                                             
+                                         }
+                                         .padding()
+                                         
+                                     } // MARK: LABEL
+                                     
+                                 }
+                             }
+                         }
+                         
+                     } // MARK: VSTACK SERIES
+                     .navigationTitle("Series")
+                 }
+                 
+             }
+             
+             .task {
+                 await seriesVM.fetchAllTVSeries()
+             }
+             .ignoresSafeArea()
+         } // MARK: Primeira VSTACK
+         .ignores
+ */
 
 
 //struct ScrollMovies_Previews: PreviewProvider {
--- NanoChallengeWebService/NanoChallengeWebService/Core/Movies/View/MovieHomeView.swift
+++ NanoChallengeWebService/NanoChallengeWebService/Core/Movies/View/MovieHomeView.swift
@@ -15,6 +15,7 @@ struct MovieHomeView: View {
     @State var movieService: MovieService = MovieService()
     
     var body: some View {
+        VStack {
         NavigationView {
             ScrollView {
                 VStack {
@@ -50,7 +51,7 @@ struct MovieHomeView: View {
                         print("Error: \(error)")
                     }
                 }
-                
+            }
             }
         }
     }
@@ -62,4 +63,80 @@ struct MovieHomeView_Previews: PreviewProvider {
     }
 }
 
-
+//
+//var body: some View {
+//        VStack {
+//            NavigationStack{
+//                ScrollView {
+//                    VStack {
+//                        HStack {
+//                            Text("Series")
+//                                .font(.title)
+//                                .padding(.horizontal)
+//                            Spacer()
+//                            Text("Ver todas")
+//                                .font(.title2)
+//                                .padding(.horizontal)
+//                        }
+//                        ScrollView(.horizontal, showsIndicators: false){
+//                            LazyHStack {
+//                                ForEach(0 ..< seriesVM.tvSeriesArray.count, id: \.self){ index in
+//                                    NavigationLink {
+//                                        TVSerieDetail(series: seriesVM.tvSeriesArray[index])
+//
+//                                    } label: {
+//                                        VStack {
+//                                            // if let para pegar o complemento da imagem do link
+//                                            if let backDropPath = seriesVM.tvSeriesArray[index].backdrop_path {
+//                                                AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/original\(backDropPath)")) {
+//                                                    image in
+//                                                    image
+//                                                        .resizable()
+//                                                        .scaledToFill()
+//                                                        .backgroundStyle(.white)
+//                                                        .frame(maxWidth: 185,maxHeight: 230)
+//                                                        .cornerRadius(16)
+//                                                        .shadow(radius: 8, x: 5, y: 5)
+//                                                        .overlay {
+//                                                            RoundedRectangle(cornerRadius: 16)
+//                                                                .stroke(.gray.opacity(0.5), lineWidth: 1)
+//                                                        }
+//                                                } placeholder: {
+//                                                    RoundedRectangle(cornerRadius: 16)
+//                                                        .stroke(.gray.opacity(0.5), lineWidth: 1)
+//                                                        .frame(width: 160, height: 230)
+//                                                        .overlay {
+//                                                            ProgressView()
+//                                                                .scaleEffect(4)
+//                                                                .frame(maxHeight: 96)
+//                                                                .padding()
+//                                                        }
+//                                                }
+//
+//                                            }
+//                                            Text("\(seriesVM.tvSeriesArray[index].name.capitalized)")
+//                                                .font(.title2)
+//                                                .foregroundColor(.black)
+//                                            Text("\(seriesVM.count2)")
+//
+//                                        }
+//                                        .padding()
+//
+//                                    } // MARK: LABEL
+//
+//                                }
+//                            }
+//                        }
+//
+//                    } // MARK: VSTACK SERIES
+//                    .navigationTitle("Series")
+//                }
+//
+//            }
+//
+//            .task {
+//                await seriesVM.fetchAllTVSeries()
+//            }
+//            .ignoresSafeArea()
+//        } // MARK: Primeira VSTACK
+//        .ignores
