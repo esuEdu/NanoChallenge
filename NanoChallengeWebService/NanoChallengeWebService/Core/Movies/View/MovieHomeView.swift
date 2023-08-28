41bdb3be35bb29b29d98bd67f8537513d3ea23c9
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2023-08-28 14:14:26 -0300

    Tentando arrumar com o edu

--- NanoChallengeWebService/NanoChallengeWebService/Core/Movies/View/MovieHomeView.swift
+++ NanoChallengeWebService/NanoChallengeWebService/Core/Movies/View/MovieHomeView.swift
@@ -2,53 +2,119 @@ import SwiftUI
 
 ///View que exibe os filmes
 struct MovieHomeView: View {
-    @State var movieService: MovieService = MovieService()  //Classe que contém lógica de requisições de api.
-
-    @State var responseMovieDiscover: responceDiscoverMovies? //Recebe da API filmes recentes
-    @State var moviesAdventure: responceDiscoverMovies? //Recebe da API filmes de aventura
-    @State var moviesRomance: responceDiscoverMovies? //Recebe da API filmes de romance
-
-//    @StateObject var viewModel:MovieHomeViewModel = MovieHomeViewModel()
+//    @State var movieService: MovieService = MovieService()  //Classe que contém lógica de requisições de api.
+//
+//    @State var responseMovieDiscover: responceDiscoverMovies? //Recebe da API filmes recentes
+//    @State var moviesAdventure: responceDiscoverMovies? //Recebe da API filmes de aventura
+//    @State var moviesRomance: responceDiscoverMovies? //Recebe da API filmes de romance
+//    @State var search: String = ""
+//    @State var isModalVisible = false
+    @StateObject var mv:MovieHomeViewModel = MovieHomeViewModel()
 
     var body: some View {
         VStack {
-        NavigationView {
-            ScrollView {
-                VStack {
-
-                    //MARK: Filmes recentes
-                    HearderSrollMovie(title: "Movies") //Header do ScrollMovies (título e "All Movies")
-                    ScrollMovies(movies:  responseMovieDiscover?.results ?? []) //ScrollMovies exibindo todos os filmes.
+            NavigationView {
                 
-                    //MARK: Filmes de ação
-                    HearderSrollMovie(title: GenreMovie.adventure.name) //GenreMovie.adventure.name = "adventure"
-                    ScrollMovies(movies:  moviesAdventure?.results ?? [])
-                    
-                    //MARK: Filmes de romance
-                    HearderSrollMovie(title: GenreMovie.romance.name)   //GenreMovie.romance.name = "romance"
-
-                    ScrollMovies(movies:  moviesRomance?.results ?? [])
-                    
-                    
-                }
-                .navigationTitle("Movies")
-                .task { //Chamando API
-                    do {
-                         responseMovieDiscover = try await  movieService.getDiscoverMovies()
-
-                         moviesAdventure = try await  movieService.getDiscoverMoviesGenre(idGenre: GenreMovie.adventure.rawValue)
+                ScrollView {
+//
+//                    TextField("Search", text: $search)
+//                        .padding()
+//
+//                    Button(action: {
+//                        // Use um bloco async para tratar a chamada assíncrona
+//                        Task {
+//                            do {
+//                                let movies = try await movieService.getMoviesByWorld(search: search)
+//                            } catch {
+//                                print("Error fetching movies: \(error)")
+//                            }
+//                        }
+//                    }) {
+//                        Text("Search Movies")
+//                            .padding()
+//                            .background(Color.blue)
+//                            .foregroundColor(.white)
+//                            .cornerRadius(10)
+//                    }
+//                    VStack {
                         
-                         moviesRomance = try await  movieService.getDiscoverMoviesGenre(idGenre: GenreMovie.romance.rawValue)
-
-                    } catch {
-                        print("Error: \(error)")
+                        //MARK: Filmes recentes
+                        HearderSrollMovie(title: "Movies") //Header do ScrollMovies (título e "All Movies")
+                        ScrollMovies(movies:  responseMovieDiscover?.results ?? []) //ScrollMovies exibindo todos os filmes.
+                        
+                        //MARK: Filmes de ação
+                        HearderSrollMovie(title: GenreMovie.adventure.name) //GenreMovie.adventure.name = "adventure"
+                        ScrollMovies(movies:  moviesAdventure?.results ?? [])
+                        
+                        //MARK: Filmes de romance
+                        HearderSrollMovie(title: GenreMovie.romance.name)   //GenreMovie.romance.name = "romance"
+                        
+                        ScrollMovies(movies: moviesRomance?.results ?? [])
+                        
+                        
+                    }
+                    .navigationTitle("Movies")
+                    .task { //Chamando API
+                        do {
+                            responseMovieDiscover = try await  movieService.getDiscoverMovies()
+                            
+                            moviesAdventure = mv.getMoviesByGenrer(idGenre: GenreMovie.adventure.rawValue)
+                            
+                            moviesRomance = try await  movieService.getDiscoverMoviesGenre(idGenre: GenreMovie.romance.rawValue)
+                            
+                        } catch {
+                            print("Error: \(error)")
+                        }
                     }
                 }
-            }
+//                .background(
+//                    ZStack {
+//                        if let cast = selectedMovie?.cast, isModalVisible {
+//                            Color.black.opacity(0.5)
+//                                .ignoresSafeArea()
+//                                .onTapGesture {
+//                                    isModalVisible = false
+//                                }
+//
+//                            VStack {
+//                                Text("Cast Members")
+//                                    .font(.title)
+//                                    .padding()
+//
+//                                ScrollView {
+//                                    ForEach(cast) { member in
+//                                        VStack(alignment: .leading) {
+//                                            Text(member.name)
+//                                                .font(.headline)
+//                                            Text("Character: \(member.character ?? "N/A")")
+//                                                .font(.subheadline)
+//                                        }
+//                                        .padding()
+//                                    }
+//                                }
+//
+//                                Button("Close") {
+//                                    isModalVisible = false
+//                                }
+//                                .padding()
+//                                .background(Color.blue)
+//                                .foregroundColor(.white)
+//                                .cornerRadius(10)
+//                            }
+//                            .padding()
+//                            .background(Color.white)
+//                            .cornerRadius(15)
+//                            .shadow(radius: 5)
+//                            .offset(y: isModalVisible ? 0 : UIScreen.main.bounds.height)
+//                            .animation(.spring())
+//                        }
+//                    }
+//                )
+
             }
         }
     }
-}
+//}
 
 struct MovieHomeView_Previews: PreviewProvider {
     static var previews: some View {
--- NanoChallengeWebService/NanoChallengeWebService/Core/Movies/ViewModel/MovieItemViewModel.swift
+++ NanoChallengeWebService/NanoChallengeWebService/Core/Movies/ViewModel/MovieItemViewModel.swift
@@ -8,12 +8,72 @@
 import Foundation
 import SwiftUI
 
-//class MovieHomeViewModel: ObservableObject {
-//    @Published var movieService: MovieService = MovieService()
-//    @Published var responseMovieDiscover: responceDiscoverMovies?
-//    @Published var moviesAdventure: responceDiscoverMovies?
-//    @Published var moviesRomance: responceDiscoverMovies?
-//}
+class MovieHomeViewModel: ObservableObject {
+    private var service = MovieService()
+    @Published var responseMovieDiscover: responceDiscoverMovies?
+    @Published var moviesAdventure: responceDiscoverMovies?
+    @Published var moviesRomance: responceDiscoverMovies?
+    
+    func getMovie(idMovie: Int) async throws {
+        do {
+            let responce = try await service.getMovie(id: idMovie)
+            await MainActor.run(body: {
+                
+            })
+        }
+    }
+    
+    func getDiscoverMovies() async throws {
+        do {
+            let responce = try await service.getDiscoverMovies()
+            await MainActor.run(body: {//É aqui que colocamos dados dentro da published
+                responseMovieDiscover = responce
+            })
+        }
+    }
+    
+    /*
+     criar uma unção para cada tipo de filme
+     */
+//    func getMoviesByGenrer(idGenre: Int) async throws {
+//        do {
+//            let responce = try await service.getDiscoverMoviesGenre(idGenre: idGenre)
+//            await MainActor.run(body: {
+//                moviesAdventure = responce
+//            })
+//        }
+//    }
+    
+        func getAdventure() async throws {
+            do {
+                let responce = try await service.getDiscoverMoviesGenre(idGenre: GenreMovie.adventure.rawValue)
+                await MainActor.run(body: {
+                    moviesAdventure = responce
+                })
+            }
+        }
+    
+        func getRomance() async throws {
+            do {
+                let responce = try await service.getDiscoverMoviesGenre(idGenre: GenreMovie.romance.rawValue)
+                await MainActor.run(body: {
+                    moviesAdventure = responce
+                })
+            }
+        }
+    
+    func getMoviesByWorld(search: String) async throws {
+        do {
+            let responce = try await service.getMoviesByWorld(search: search)
+            await MainActor.run(body: {
+                responseMovieDiscover = responce
+            })
+        }
+    }
+
+    
+    
+}
 
 
 ///Classe que contém todas as funções de consulta de api
