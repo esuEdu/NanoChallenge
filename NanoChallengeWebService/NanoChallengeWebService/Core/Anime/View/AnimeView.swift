6ccdaa28727b76b111e4bb246a5590d3991886de
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-08-29 11:59:18 -0300

    chaves

--- NanoChallengeWebService/NanoChallengeWebService/Core/Anime/View/AnimeView.swift
+++ NanoChallengeWebService/NanoChallengeWebService/Core/Anime/View/AnimeView.swift
@@ -44,74 +44,72 @@ struct AnimeView: View {
                             .frame(width: UIScreen.main.bounds.width, height: reader.frame(in: .global).minY + 480)
                     }
                 }.frame(height: 480)
-                
-                VStack{
-                    HStack(alignment: .top) {
+                HStack(alignment: .top) {
+                    
+                    
+                    Button{
+                        let test = Favorito(context: dataController.container.viewContext)
+                        test.id = String(anime.id)
+                        test.type = "anime"
+                        if !dataController.favoritos.contains(test){
+                            dataController.addFavorite(id: String(anime.id), type: "anime")
+                            viewModel.isFavorite.toggle()
+                        }
                         
+                    }label: {
                         
-                        Button{
-                            let test = Favorito(context: dataController.container.viewContext)
-                            test.id = String(anime.id)
-                            test.type = "anime"
-                            if !dataController.favoritos.contains(test){
-                                dataController.addFavorite(id: String(anime.id), type: "anime")
-                                viewModel.isFavorite.toggle()
-                            }
-
-                        }label: {
-                            
-                            if !dataController.favoritos.contains(where: { i in
-                                i.id == String(anime.id)
-                            }) || viewModel.isFavorite == true{
-                                Image(systemName: "heart")
+                        if !dataController.favoritos.contains(where: { i in
+                            i.id == String(anime.id)
+                        }) || viewModel.isFavorite == true{
+                            Image(systemName: "heart")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 26, height: 26)
+                                .foregroundColor(Color(red: 0.97, green: 0.48, blue: 0.33))
+                        }else{
+                            Image(systemName: "heart.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 26, height: 26)
+                                .foregroundColor(Color(red: 0.97, green: 0.48, blue: 0.33))
+                        }
+                    }
+                    
+                    
+                    VStack(alignment: .leading, spacing: 15) {
+                        
+                        Text((anime.title.english ?? anime.title.romaji ?? anime.title.native) ?? "No name")
+                            .font(.system(size: 35, weight: .bold))
+                            .foregroundColor(.white)
+                        
+                        HStack {
+                            ForEach(0..<Int(viewModel.anime.first?.averageScore ?? 0)/20, id: \.self){
+                                image in
+                                Image("fillStar")
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 26, height: 26)
-                                    .foregroundColor(Color(red: 0.97, green: 0.48, blue: 0.33))
-                            }else{
-                                Image(systemName: "heart.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 26, height: 26)
-                                    .foregroundColor(Color(red: 0.97, green: 0.48, blue: 0.33))
                             }
                         }
                         
+                        Text("Genre:")
+                            .padding(.top, 0.5)
+                        let genres = viewModel.anime.first?.genres
+                        let allgenres = genres?.joined(separator: ", ")
+                        Text(allgenres ?? "")
+                        
+                        Text("average Score:")
+                            .padding(.top, 0.5)
+                        Text(String((viewModel.anime.first?.averageScore ?? 0)))
+                        
+                        Text("episodes:")
+                            .padding(.top, 0.5)
+                        Text(String((viewModel.anime.first?.episodes ?? 0)))
+                    }
+                    .padding(.top, 8)
+                    Spacer()
                     
-                        VStack(alignment: .leading, spacing: 15) {
-                            
-                            Text((anime.title.english ?? anime.title.romaji ?? anime.title.native) ?? "No name")
-                                .font(.system(size: 35, weight: .bold))
-                                .foregroundColor(.white)
-                            
-                            HStack {
-                                ForEach(0..<Int(viewModel.anime.first?.averageScore ?? 0)/20, id: \.self){
-                                    image in
-                                    Image("fillStar")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 26, height: 26)
-                                }
-                            }
-                                
-                            Text("Genre:")
-                                .padding(.top, 0.5)
-                            let genres = viewModel.anime.first?.genres
-                            let allgenres = genres?.joined(separator: ", ")
-                            Text(allgenres ?? "")
-                            
-                            Text("average Score:")
-                                .padding(.top, 0.5)
-                            Text(String((viewModel.anime.first?.averageScore ?? 0)))
-                            
-                            Text("episodes:")
-                                .padding(.top, 0.5)
-                            Text(String((viewModel.anime.first?.episodes ?? 0)))
-                        }
-                        .padding(.top, 8)
-                        Spacer()
-                    
-                    .padding()
+                        .padding()
                     
                     VStack(alignment: .leading) {
                         if let description = viewModel.anime.first?.description {
@@ -153,154 +151,3 @@ struct AnimeView: View {
     }
 }
 
-//struct AnimeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        AnimeView(anime: )
-//    }
-//}
-
-//struct TVSerieDetail: View {
-//    @StateObject var seriesDetailVM = SeriesDetailViewlModel()
-//    var series: TVSeries
-//
-//
-//    var body: some View {
-//
-//        ScrollView(.vertical, showsIndicators: false){
-//            GeometryReader{ reader in
-//                if let backDropPath = series.backdrop_path{
-//                    AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/original\(backDropPath)"))  {
-//                        image in
-//                        image
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fill)
-//                            .offset(y: -reader.frame(in: .global).minY)
-//                            .frame(width: UIScreen.main.bounds.width, height: reader.frame(in: .global).minY + 480)
-//
-//
-//                    } placeholder: {
-//                        ProgressView()
-//                        ProgressView()
-//                            .scaleEffect(4)
-//                            .frame(maxHeight: 96)
-//                            .foregroundColor(.white)
-//                            .offset(y: -reader.frame(in: .global).minY)
-//                            .frame(width: UIScreen.main.bounds.width, height: reader.frame(in: .global).minY + 480)
-//                    }
-//                }
-//            }
-//            .frame(height: 480)
-//            VStack(alignment: .leading, spacing: 15){
-//                Text(series.name)
-//                    .font(.system(size: 35, weight: .bold))
-//                    .foregroundColor(.white)
-//                // Listar as estrelas
-//                HStack {
-//                    ForEach(0..<Int(seriesDetailVM.vote_average), id: \.self){
-//                        image in
-//                        Image("fillStar")
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 26, height: 26)
-//                    }
-//                    // Verificar se é maior que 0.50 para por a estrela meia bomba
-//                    if (seriesDetailVM.vote_average - Double(Int(seriesDetailVM.vote_average))) >= 0.5 {
-//                        Image("semiFillStar")
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 26, height: 26)
-//                    }
-//                    // Colocar o resto das estrelas - verifica se ja existe uma entrela meia boma
-//                    if (seriesDetailVM.vote_average - Double(Int(seriesDetailVM.vote_average))) >= 0.5 {
-//                        ForEach(0...(3 - Int(seriesDetailVM.vote_average)), id: \.self) {
-//                            image in
-//                            Image("notFillStar")
-//                                .resizable()
-//                                .scaledToFit()
-//                                .frame(width: 26, height: 26)
-//                        }
-//                        // caso nao tenha ele bota todas normal, caso tenha ele bota uma a menos
-//                    } else {
-//                        ForEach(0...(4 - Int(seriesDetailVM.vote_average)), id: \.self) {
-//                            image in
-//                            Image("notFillStar")
-//                                .resizable()
-//                                .scaledToFit()
-//                                .frame(width: 26, height: 26)
-//                        }
-//                    }
-//                    Spacer()
-//                    // Botaão de favoritos
-//                    Button {
-//
-//                    } label: {
-//                        Image("corazon")
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 26, height: 26)
-//                    }
-//
-//                }
-//
-//
-//                // Listar todos os generos
-//                HStack {
-//                    ForEach(seriesDetailVM.genresTV, id: \.id){
-//                        genre in
-//                            GenresDesign(name: genre.name)
-//                    }
-//                }
-//                // verifica se exisgte descricao no retorno da API
-//                if seriesDetailVM.overview == ""{
-//                    Text("Description not found")
-//
-//                        .foregroundColor(Color("TextColor"))
-//                } else {
-//                    Text(seriesDetailVM.overview)
-//
-//                        .foregroundColor(Color("TextColor"))
-//                }
-//
-//                //Número de Temporadas
-//                Text("Temporadas - \(seriesDetailVM.number_of_seasons)")
-//                    .font(.system(size: 24, weight: .bold))
-//                    .foregroundColor(.white)
-//                //Número de episódios
-//                Text("Episódios - \(seriesDetailVM.number_of_episodes)")
-//                    .font(.system(size: 24, weight: .bold))
-//                    .foregroundColor(.white)
-//                //puxar todos os eps
-//                ForEach(seriesDetailVM.allEpisodes, id: \.self) {
-//                    episode in
-//                    Text("\(episode.name)")
-//                        .font(.system(size: 21, weight: .bold))
-//                        .foregroundColor(.white)
-//                    Text(episode.overview)
-//                        .foregroundColor(Color("TextColor"))
-//                }
-//
-//            } // MARK: VStack principal
-//        //    .frame(width: 360)
-//            .padding(.top, 25)
-//            .padding(.horizontal)
-//            .background(Color("BackGroundColor"))
-//            .cornerRadius(20)
-//            .offset(y: -35)
-//        }
-//        .edgesIgnoringSafeArea(.all)
-//        .background(Color("BackGroundColor"))
-//        .task {
-//      //      seriesDetailVM.urlString = series._links.selfLink.href
-//            do {
-//                try await seriesDetailVM.fetchTVSerie(id: series.id)
-//                try await seriesDetailVM.fetchAllEpisode(idSerie: series.id, season: 1)
-//            } catch {
-//                print("Erro ao puxar os dados do detalhe")
-//                if let decodingError = error as? DecodingError {
-//                       print("Decoding error: \(decodingError)")
-//                   }
-//
-//            }
-//        }
-//    }
-//}
